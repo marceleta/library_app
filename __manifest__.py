9cd404b00d773e103217fd6e410936d8b42679e0
marcelo <marcelo@debian-VirtualBox.virtualbox>
2020-07-27 13:38:41 -0300

    primeiro commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Library Mangement',
+    'description': 'Manage library book catalogue and lending',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+    'data':[
+        'views/library_menu.xml',
+        ],
+}
