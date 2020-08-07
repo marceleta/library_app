9eb7dd41e724417398aa88d63699bbf946b49629
Marcelo Paes Rocha <marcelo.rocha@nextsolucoes.net.br>
2020-08-07 17:24:31 -0300

    finalizado

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,14 @@
     'application': True,
     'data':[
         'security/library_security.xml',
+        'security/ir.model.access.csv',
         'views/library_menu.xml',
+        'views/book_view.xml',
+        'views/book_list_template.xml',
         ],
+    'demo':[
+        'data/res.partner.csv',
+        'data/library.book.csv',
+        'data/book_demo.xml'
+        ]
 }
